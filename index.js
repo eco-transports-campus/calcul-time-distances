7fc8881433a0a01cb45c1b88f95d80f6bf1eb0d6
Anuswan91 <vivien.blum@free.fr>
2018-04-12 23:03:56 +0200

    Done function

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-exports.getDistances = function (var origin, var destination, var key) {
-	var data = [];
-	var modes = {"driving","walking","bicycling"};
+exports.getDistances = (origins, destinations, mode, keyAPI) => {
+  const url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+  const urlAPI = `${url}mode=${mode}&origins=${origins}&destinations=${destinations}&key=${keyAPI}`
 
-	for (var mode in modes){ 
-		axios.get('https://maps.googleapis.com/maps/api/distancematrix/json?mode=' + mode +'origins=' + origin + '&destinations=' + destination + '&key=' + key)
-		  .then(response => {
-			let answer = JSON.parse(response.data);
-			var info = {};
-			if (answer.status == "OK")
-			{
-				if (answer.rows[0].elements[0].status == "NOT_FOUND")
-				{
-					info['status'] = "NOT_FOUND";
-					console.warn("Warning: Distance API didn't found an itinerary (mode: " + mode + ",origin: " + origin + ", destination: " + destination + ")");
-				}
-				if (answer.rows[0].elements[0].status == "OK")
-				{
-					info['status'] = "OK";
-					info['distance'] = answer.rows[0].elements[0].distance.value;
-					info['duration'] = answer.rows[0].elements[0].duration.value;
-				}
-			}
-			else{
-				console.warn("Warning: Distance API respond with a status: " + answer.status + "(mode: " + mode + ",origin: " + origin + ", destination: " + destination + ")");
-				info['status'] = "KO";
-			}
-			info['mode'] = mode;
-			data.push(info);
-		  })
-		  .catch(error => {
-			console.error(error);
-			return null;
-		  });
-	}
-	
-	return data;
-};
+  return new Promise((resolve, reject) => {
+    axios
+      .get(urlAPI)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
